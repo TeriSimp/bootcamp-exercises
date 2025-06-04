e7122cbb72a819e6cea045f8d9a78e2b6b54568a
TeriSimp <154432286+TeriSimp@users.noreply.github.com>
2025-06-04 20:58:13 +0700

    baitap_node.js

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,9 @@
+import generateName from "sillyname";
+const sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`);
+
+import {randomSuperhero} from 'superheroes';
+const heroName = randomSuperhero();
+
+console.log(`My superhero name is ${heroName}.`);
