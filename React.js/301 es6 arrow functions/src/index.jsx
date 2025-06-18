3b55021c3e12f63f363e78f61b85b6503ace5805
TeriSimp <154432286+TeriSimp@users.noreply.github.com>
2025-06-18 15:52:32 +0700

    baitap_301

---
+++ React.js/301 es6 arrow functions/src/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// var numbers = [3, 56, 2, 48, 5];
+
+// Map -Create a new array by doing something with each item in an array.
+// const mapNumbers = numbers.map( x => x * 2);
+
+// Filter - Create a new array by keeping the items that return true.
+// const filterNumbers = numbers.filter( num => num < 10);
+
+// Reduce - Accumulate a value by doing something to each item in an array.
+// const reduceNumbers = numbers.redice( accumulator, currentNumber => accumulator + currentNumber);
+
+// Find - find the first item that matches from an array.
+// const findNumbers = numbers.find( num => num > 10);
+
+// FindIndex - find the index of the first item that matches.
+// const findIndexNumbers = numbers.findIndex( num => num > 10);
